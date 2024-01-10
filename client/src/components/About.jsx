dfb5a7443b2f514fc9a09b643f518a0b071fcf76
adhaamtarekk <63559019+adhaamtarekk@users.noreply.github.com>
2024-01-10 05:39:50 +0200

    2nd

---
+++ client/src/components/About.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+import { Tilt } from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ServiceCard = ({ index, title, icon }) => (
+  <Tilt className="xs:w-[250px] w-full">
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+      >
+        <img
+          src={icon}
+          alt="web-development"
+          className="w-16 h-16 object-contain"
+        />
+
+        <h3 className="text-white text-[20px] font-bold text-center">
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  </Tilt>
+);
+
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+      >
+        I am a Computer Engineering graduate from the German University in
+        Cairo, equipped with comprehensive skills as a software developer. My
+        expertise lies in the MERN stack, Flutter, and proficiency in frameworks
+        such as React, Express.js, and Flutter. With a strong commitment to
+        continuous learning, I adapt quickly to new technologies and stay
+        up-to-date with industry trends. I thrive on collaborating closely with
+        clients, ensuring a deep understanding of their requirements and
+        leveraging my technical prowess to create efficient, scalable, and
+        user-friendly solutions that effectively address real-world problems. My
+        goal is to bring your ideas to life by delivering high-quality software
+        solutions that exceed expectations. Let's embark on a collaborative
+        journey and transform your vision into reality.
+      </motion.p>
+
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About, "about");
